b874b1904b600ea5d1ea5979644fabeff58b5d52
mayurcj7 <100471166+mayurcj7@users.noreply.github.com>
2022-12-21 15:18:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
-    ApplicationEnv = "aaa"
+    Name = "ExampleAppServerInstance",
+    ApplicationEnv = "aaa",
     Environment = "bbb"
   }
 }
